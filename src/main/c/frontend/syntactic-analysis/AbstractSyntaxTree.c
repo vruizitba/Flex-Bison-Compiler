9676ce3d254b130a28e25ec10ce8ec794fc06927
sallegretti <santiagoallegretti@gmail.com>
2026-05-01 18:30:18 -0300

    feat: design AST types

--- src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.c
+++ src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.c
@@ -68,3 +68,180 @@ void destroyProgram(Program * program) {
 		free(program);
 	}
 }
+
+/**
+ * Our DSL types destructors. For now, we won't delete the previous ones.
+ */
+
+void destroyType(Type * type) {
+	if (type == NULL) {
+		return;
+	}
+	free(type->className);
+	destroyType(type->inner);
+	free(type);
+}
+
+void destroyDslExpression(DslExpression * expression) {
+	if (expression == NULL) {
+		return;
+	}
+	switch (expression->kind) {
+		case EXPRESSION_STRING:
+			free(expression->stringValue);
+			break;
+		case EXPRESSION_IDENTIFIER:
+			free(expression->identifier);
+			break;
+		case EXPRESSION_BINARY:
+			destroyDslExpression(expression->binary.left);
+			destroyDslExpression(expression->binary.right);
+			break;
+		case EXPRESSION_UNARY:
+			destroyDslExpression(expression->unary.operand);
+			break;
+		case EXPRESSION_FIELD_ACCESS:
+		case EXPRESSION_ARROW_ACCESS:
+			destroyDslExpression(expression->fieldAccess.object);
+			free(expression->fieldAccess.field);
+			break;
+		case EXPRESSION_INDEX:
+			destroyDslExpression(expression->indexAccess.array);
+			destroyDslExpression(expression->indexAccess.index);
+			break;
+		case EXPRESSION_CALL:
+			destroyDslExpression(expression->call.callee);
+			destroyArgumentList(expression->call.arguments);
+			break;
+		case EXPRESSION_NEW:
+			free(expression->newExpression.className);
+			destroyArgumentList(expression->newExpression.arguments);
+			break;
+		default:
+			break;
+	}
+	free(expression);
+}
+
+void destroyDslStatement(DslStatement * statement) {
+	if (statement == NULL) {
+		return;
+	}
+	switch (statement->kind) {
+		case STATEMENT_VARIABLE_DECLARATION:
+			destroyType(statement->variableDeclaration.type);
+			free(statement->variableDeclaration.name);
+			destroyDslExpression(statement->variableDeclaration.initializer);
+			break;
+		case STATEMENT_EXPRESSION:
+			destroyDslExpression(statement->expressionStatement);
+			break;
+		case STATEMENT_IF:
+			destroyDslExpression(statement->ifStatement.condition);
+			destroyDslStatement(statement->ifStatement.thenBranch);
+			destroyDslStatement(statement->ifStatement.elseBranch);
+			break;
+		case STATEMENT_WHILE:
+			destroyDslExpression(statement->whileStatement.condition);
+			destroyDslStatement(statement->whileStatement.body);
+			break;
+		case STATEMENT_FOR:
+			destroyDslStatement(statement->forStatement.initializer);
+			destroyDslExpression(statement->forStatement.condition);
+			destroyDslExpression(statement->forStatement.step);
+			destroyDslStatement(statement->forStatement.body);
+			break;
+		case STATEMENT_RETURN:
+			destroyDslExpression(statement->returnStatement.value);
+			break;
+		case STATEMENT_BLOCK:
+			destroyStatementList(statement->block);
+			break;
+	}
+	free(statement);
+}
+
+void destroyStatementList(StatementList * list) {
+	if (list == NULL) {
+		return;
+	}
+	destroyDslStatement(list->statement);
+	destroyStatementList(list->next);
+	free(list);
+}
+
+void destroyArgumentList(ArgumentList * list) {
+	if (list == NULL) {
+		return;
+	}
+	destroyDslExpression(list->expression);
+	destroyArgumentList(list->next);
+	free(list);
+}
+
+void destroyParameter(Parameter * parameter) {
+	if (parameter == NULL) {
+		return;
+	}
+	destroyType(parameter->type);
+	free(parameter->name);
+	destroyParameter(parameter->next);
+	free(parameter);
+}
+
+void destroyField(Field * field) {
+	if (field == NULL) {
+		return;
+	}
+	destroyType(field->type);
+	free(field->name);
+	destroyDslExpression(field->initializer);
+	destroyField(field->next);
+	free(field);
+}
+
+void destroyMethod(Method * method) {
+	if (method == NULL) {
+		return;
+	}
+	destroyType(method->returnType);
+	free(method->name);
+	destroyParameter(method->parameters);
+	destroyStatementList(method->body);
+	destroyMethod(method->next);
+	free(method);
+}
+
+void destroyClass(Class * class) {
+	if (class == NULL) {
+		return;
+	}
+	free(class->name);
+	free(class->parentName);
+	destroyField(class->fields);
+	destroyMethod(class->methods);
+	destroyClass(class->next);
+	free(class);
+}
+
+void destroyFunction(Function * function) {
+	if (function == NULL) {
+		return;
+	}
+	destroyType(function->returnType);
+	free(function->name);
+	destroyParameter(function->parameters);
+	destroyStatementList(function->body);
+	destroyFunction(function->next);
+	free(function);
+}
+
+void destroyDslProgram(DslProgram * program) {
+	if (program == NULL) {
+		return;
+	}
+	destroyClass(program->classes);
+	destroyFunction(program->functions);
+	destroyStatementList(program->mainBody);
+	free(program);
+}
--- src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
+++ src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
@@ -75,4 +75,215 @@ void destroyExpression(Expression * expression);
 void destroyFactor(Factor * factor);
 void destroyProgram(Program * program);
 
+/**
+ * Our DSL types. For now, we won't delete the previous types.
+ */
+
+typedef enum {
+    VISIBILITY_PUBLIC,
+    VISIBILITY_PRIVATE,
+    VISIBILITY_PROTECTED
+} Visibility;
+
+typedef enum {
+    TYPE_INT, TYPE_BOOL, TYPE_STRING, TYPE_CHAR,
+    TYPE_FLOAT, TYPE_DOUBLE, TYPE_VOID,
+    TYPE_CLASS, TYPE_POINTER, TYPE_ARRAY
+} TypeKind;
+
+typedef enum {
+    BINARY_OPERATOR_ADD, BINARY_OPERATOR_SUB, BINARY_OPERATOR_MUL, BINARY_OPERATOR_DIV, BINARY_OPERATOR_MOD,
+    BINARY_OPERATOR_EQUAL, BINARY_OPERATOR_NOT_EQUAL,
+    BINARY_OPERATOR_LESS, BINARY_OPERATOR_GREATER, BINARY_OPERATOR_LESS_EQUAL, BINARY_OPERATOR_GREATER_EQUAL,
+    BINARY_OPERATOR_AND, BINARY_OPERATOR_OR,
+    BINARY_OPERATOR_ASSIGN, BINARY_OPERATOR_PLUS_ASSIGN, BINARY_OPERATOR_MINUS_ASSIGN,
+    BINARY_OPERATOR_MUL_ASSIGN, BINARY_OPERATOR_DIV_ASSIGN, BINARY_OPERATOR_MOD_ASSIGN
+} DslBinaryOperator;
+
+typedef enum {
+    UNARY_OPERATOR_NOT, UNARY_OPERATOR_NEGATE,
+    UNARY_OPERATOR_PRE_INCREMENT, UNARY_OPERATOR_PRE_DECREMENT,
+    UNARY_OPERATOR_POST_INCREMENT, UNARY_OPERATOR_POST_DECREMENT,
+    UNARY_OPERATOR_DEREFERENCE, UNARY_OPERATOR_ADDRESS_OF
+} DslUnaryOperator;
+
+typedef enum {
+    EXPRESSION_INTEGER, EXPRESSION_FLOAT, EXPRESSION_STRING,
+    EXPRESSION_CHAR, EXPRESSION_BOOLEAN,
+    EXPRESSION_IDENTIFIER, EXPRESSION_THIS, EXPRESSION_NEW,
+    EXPRESSION_BINARY, EXPRESSION_UNARY,
+    EXPRESSION_FIELD_ACCESS, EXPRESSION_ARROW_ACCESS,
+    EXPRESSION_INDEX, EXPRESSION_CALL
+} ExpressionKind;
+
+typedef enum {
+    STATEMENT_VARIABLE_DECLARATION, STATEMENT_IF, STATEMENT_WHILE,
+    STATEMENT_FOR, STATEMENT_RETURN, STATEMENT_EXPRESSION, STATEMENT_BLOCK
+} StatementKind;
+
+typedef struct Type Type;
+typedef struct DslExpression DslExpression;
+typedef struct DslStatement DslStatement;
+typedef struct Parameter Parameter;
+typedef struct StatementList StatementList;
+typedef struct ArgumentList ArgumentList;
+typedef struct Method Method;
+typedef struct Field Field;
+typedef struct Class Class;
+typedef struct Function Function;
+typedef struct DslProgram DslProgram;
+
+struct Type {
+    TypeKind kind;
+    char isSigned;
+    char isUnsigned;
+    char isShort;
+    char isLong;
+    char * className;   // TYPE_CLASS
+    Type * inner;       // TYPE_POINTER, TYPE_ARRAY
+    int arraySize;      // TYPE_ARRAY (-1 = no size specified)
+};
+
+void destroyType(Type * type);
+
+struct DslExpression {
+    ExpressionKind kind;
+    union {
+        int integerValue;
+        double floatValue;
+        char * stringValue;
+        char charValue;
+        int booleanValue;
+        char * identifier;
+        struct {
+            DslBinaryOperator operator;
+            DslExpression * left;
+            DslExpression * right;
+        } binary;
+        struct {
+            DslUnaryOperator operator;
+            DslExpression * operand;
+        } unary;
+        struct {
+            DslExpression * object;
+            char * field;
+        } fieldAccess;
+        struct {
+            DslExpression * array;
+            DslExpression * index;
+        } indexAccess;
+        struct {
+            DslExpression * callee;
+            ArgumentList * arguments;
+        } call;
+        struct {
+            char * className;
+            ArgumentList * arguments;
+        } newExpression;
+    };
+};
+
+void destroyDslExpression(DslExpression * expression);
+
+struct DslStatement {
+    StatementKind kind;
+    union {
+        struct {
+            Type * type;
+            char * name;
+            DslExpression * initializer;    // NULL if no initializer
+        } variableDeclaration;
+        DslExpression * expressionStatement;
+        struct {
+            DslExpression * condition;
+            DslStatement * thenBranch;
+            DslStatement * elseBranch;      // NULL if no else
+        } ifStatement;
+        struct {
+            DslExpression * condition;
+            DslStatement * body;
+        } whileStatement;
+        struct {
+            DslStatement * initializer;     // NULL if empty
+            DslExpression * condition;
+            DslExpression * step;
+            DslStatement * body;
+        } forStatement;
+        struct {
+            DslExpression * value;          // NULL for bare return;
+        } returnStatement;
+        StatementList * block;
+    };
+};
+
+void destroyDslStatement(DslStatement * statement);
+
+struct StatementList {
+    DslStatement * statement;
+    StatementList * next;
+};
+
+struct ArgumentList {
+    DslExpression * expression;
+    ArgumentList * next;
+};
+
+struct Parameter {
+    Type * type;
+    char * name;
+    Parameter * next;
+};
+
+void destroyStatementList(StatementList * list);
+void destroyArgumentList(ArgumentList * list);
+void destroyParameter(Parameter * parameter);
+
+struct Field {
+    Visibility visibility;
+    char isStatic;
+    Type * type;
+    char * name;
+    DslExpression * initializer;    // NULL if no initializer
+    Field * next;
+};
+
+struct Method {
+    Visibility visibility;
+    char isStatic;
+    char isConstructor;
+    Type * returnType;              // NULL if constructor
+    char * name;
+    Parameter * parameters;
+    StatementList * body;
+    Method * next;
+};
+
+struct Class {
+    char * name;
+    char * parentName;              // NULL if no extends
+    Field * fields;
+    Method * methods;
+    Class * next;
+};
+
+struct Function {
+    Type * returnType;
+    char * name;
+    Parameter * parameters;
+    StatementList * body;
+    Function * next;
+};
+
+struct DslProgram {
+    Class * classes;
+    Function * functions;
+    StatementList * mainBody;
+};
+
+void destroyField(Field * field);
+void destroyMethod(Method * method);
+void destroyClass(Class * class);
+void destroyFunction(Function * function);
+void destroyDslProgram(DslProgram * program);
+
 #endif
