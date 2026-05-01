29bef3cb1254dafc10cc1920231fa741c547736c
Valen Ruiz <vruiz@itba.edu.ar>
2026-05-01 17:19:26 -0300

    comment backend

--- src/main/c/EntryPoint.c
+++ src/main/c/EntryPoint.c
@@ -36,7 +36,7 @@ const int main(const int length, const char ** arguments) {
 	if (compilationStatus == SUCCEEDED) {
 		// ----------------------------------------------------------------------------------------
 		// Beginning of the Backend... ------------------------------------------------------------
-		logDebugging(logger, "Computing expression value...");
+		/*logDebugging(logger, "Computing expression value...");
 		ComputationResult computationResult = executeCalculator(&compilerState);
 		if (computationResult.succeeded) {
 			compilerState.value = computationResult.value;
@@ -48,6 +48,7 @@ const int main(const int length, const char ** arguments) {
 		}
 		// ...end of the Backend. -----------------------------------------------------------------
 		// ----------------------------------------------------------------------------------------
+		*/
 	}
 	else {
 		logError(logger, "The syntactic-analysis phase rejects the input program.");
