720093576a21fd4b7ecca51f9da3d7164dccd656
Valen Ruiz <vruiz@itba.edu.ar>
2026-06-01 17:45:58 -0300

    feat(codegen): add Generator skeleton

--- src/main/c/EntryPoint.c
+++ src/main/c/EntryPoint.c
@@ -36,7 +36,8 @@ const int main(const int length, const char ** arguments) {
 	if (compilationStatus == SUCCEEDED) {
 		// ----------------------------------------------------------------------------------------
 		// Beginning of the Backend... ------------------------------------------------------------
-		/*logDebugging(logger, "Computing expression value...");
+		/* Legacy.
+		logDebugging(logger, "Computing expression value...");
 		ComputationResult computationResult = executeCalculator(&compilerState);
 		if (computationResult.succeeded) {
 			compilerState.value = computationResult.value;
@@ -46,9 +47,10 @@ const int main(const int length, const char ** arguments) {
 			logError(logger, "The computation phase rejects the input program.");
 			compilationStatus = FAILED;
 		}
+		*/
+		executeGenerator(&compilerState);
 		// ...end of the Backend. -----------------------------------------------------------------
 		// ----------------------------------------------------------------------------------------
-		*/
 	}
 	else {
 		logError(logger, "The syntactic-analysis phase rejects the input program.");
